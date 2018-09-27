218245df857e0c7b4b5705824915ab651ec17d13
qudanqi <42972046+qudanqi@users.noreply.github.com>
2018-09-27 10:48:02 -0400

    Add files via upload

---
+++ ica4/2_5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "mpi.h"
+#include <fstream>
+using namespace std;
+
+int main()
+{
+
+    MPI_Init(NULL, NULL);
+
+    int total_processes;
+    int current_process;
+
+    ofstream out;
+    MPI_Comm_size(MPI_COMM_WORLD, &total_processes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &current_process);
+
+    if (current_process == 0)
+    {
+    out.open("file_5_" + to_string(current_process) + ".txt");
+    out << "This is process " << current_process << " of " << total_processes <<endl;
+    out.close();
+    }
+
+    MPI_Finalize();
+    return 0;
+}
